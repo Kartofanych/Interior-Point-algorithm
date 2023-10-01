4045d421372aa54b9a5910e1e1d1335ba285c508
79196 <kartofanych@gmail.com>
2023-10-01 17:19:36 +0300

    Hot fix

--- src/Input.java
+++ src/Input.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 public class Input {
     Scanner scanner = new Scanner(System.in);
     int n = 0;
-    int m = 0;
     int[] inputC(){
         System.out.println("Print a vector of coefficients of objective function:");
         int[] C = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        n = Arrays.stream(C).filter(it -> it != 0).toArray().length;
-        m = C.length;
+        n = C.length;
         return C;
     }
 
     int[][] inputA(){
         System.out.println("Print a matrix of coefficients of constraint function:");
-        int[][] A = new int[n][m];
+        int[][] A = new int[n][n];
         for(int i = 0; i < n; i ++){
             A[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
