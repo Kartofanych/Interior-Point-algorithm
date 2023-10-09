1e77141f5984194117ecef8c2b9ac808798ae3cf
absorian <07221432bs@gmail.com>
2023-10-09 13:48:24 +0300

    standardize input

--- src/Input.java
+++ src/Input.java
@@ -13,18 +13,17 @@ public class Input {
     void processInput() {
         System.out.println(
                 "Input in format:\n" +
-                        "  C(1) ... C(n)\n" +
-                        "A(1,1) ... A(1,n)\n" +
+                        "  C[1] ... C[n]\n" +
+                        "A[1,1] ... A[1,n]\n" +
                         "       ...\n" +
-                        "A(m,1) ... A(m,n)\n" +
-                        "  b(1) ... b(m)\n" +
-                        "(Approximation)"+
-                        "(min or max)"
+                        "A[m,1] ... A[m,n]\n" +
+                        "  b[1] ... b[m]\n" +
+                        "[Approximation]"
         );
-        System.out.println("Print a vector of coefficients of objective function:");
+//        System.out.println("Print a vector of coefficients of objective function:");
         C = Arrays.stream(scanner.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
 
-        System.out.println("Print a matrix of coefficients of constraint function, vector b and approximation:");
+//        System.out.println("Print a matrix of coefficients of constraint function, vector b and approximation:");
         LinkedList<double[]> AList = new LinkedList<>();
         while (true) {
             double[] line = Arrays.stream(scanner.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
@@ -37,9 +36,9 @@ public class Input {
         }
         A = AList.toArray(new double[0][0]);
 
-        System.out.println("Print `min` if you need to minimize and `max` if you need to maximize:");
-        String minimum = scanner.nextLine();
-        if (minimum.equals("min")) isMin = true;
+//        System.out.println("Print `min` if you need to minimize and `max` if you need to maximize:");
+//        String minimum = scanner.nextLine();
+//        if (minimum.equals("min")) isMin = true;
     }
 
     public double[] getC() {
